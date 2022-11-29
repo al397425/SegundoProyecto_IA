c2f667ed4ff08777ca088e686a464b9b84cfe76a
al385773 <al385773@uji.es>
2022-11-29 22:30:49 +0100

    pathfinding y tileset implementado con grid
    uwu

---
+++ EstrategiaPorTurnos_IA/Assets/Scripts/Testing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using CodeMonkey;
+
+public class Testing : MonoBehaviour {
+
+    public TilemapVisual tilemapVisual;
+    private Tilemap tilemap;
+    private Tilemap.TilemapObject.TilemapSprite tilemapSprite;
+
+    private void Start() {
+        tilemap = new Tilemap(10, 10, 10f, Vector3.zero);
+
+        tilemap.SetTilemapVisual(tilemapVisual);
+    }
+
+    private void Update() {
+        if (Input.GetMouseButton(0)) {
+            Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
+            tilemap.SetTilemapSprite(mouseWorldPosition, tilemapSprite);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.T)) {
+            tilemapSprite = Tilemap.TilemapObject.TilemapSprite.None;
+            CMDebug.TextPopupMouse(tilemapSprite.ToString());
+        }
+        if (Input.GetKeyDown(KeyCode.Y)) {
+            tilemapSprite = Tilemap.TilemapObject.TilemapSprite.Ground;
+            CMDebug.TextPopupMouse(tilemapSprite.ToString());
+        }
+        if (Input.GetKeyDown(KeyCode.U)) {
+            tilemapSprite = Tilemap.TilemapObject.TilemapSprite.Path;
+            CMDebug.TextPopupMouse(tilemapSprite.ToString());
+        }
+        if (Input.GetKeyDown(KeyCode.I)) {
+            tilemapSprite = Tilemap.TilemapObject.TilemapSprite.Dirt;
+            CMDebug.TextPopupMouse(tilemapSprite.ToString());
+        }
+    }
+
+}
---
+++ EstrategiaPorTurnos_IA/Assets/Scripts/Tilemap.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilemap {
+    
+    private Grid<TilemapObject> grid;
+
+    public Tilemap(int width, int height, float cellSize, Vector3 originPosition) {
+        grid = new Grid<TilemapObject>(width, height, cellSize, originPosition, (g, x, y) => new TilemapObject(g, x, y));
+    }
+
+    public void SetTilemapSprite(Vector3 worldPosition, TilemapObject.TilemapSprite tilemapSprite) {
+        TilemapObject tilemapObject = grid.GetGridObject(worldPosition);
+        if (tilemapObject != null) {
+            tilemapObject.SetTilemapSprite(tilemapSprite);
+        }
+    }
+
+    public void SetTilemapVisual(TilemapVisual tilemapVisual) {
+        tilemapVisual.SetGrid(this, grid);
+    }
+    
+    public class TilemapObject {
+
+        public enum TilemapSprite {
+            None,
+            Ground,
+            Path,
+            Dirt,
+        }
+
+        private Grid<TilemapObject> grid;
+        private int x;
+        private int y;
+        private TilemapSprite tilemapSprite;
+
+        public TilemapObject(Grid<TilemapObject> grid, int x, int y) {
+            this.grid = grid;
+            this.x = x;
+            this.y = y;
+        }
+
+        public void SetTilemapSprite(TilemapSprite tilemapSprite) {
+            this.tilemapSprite = tilemapSprite;
+            grid.TriggerGridObjectChanged(x, y);
+        }
+
+        public TilemapSprite GetTilemapSprite() {
+            return tilemapSprite;
+        }
+
+        public override string ToString() {
+            return tilemapSprite.ToString();
+        }
+    }
+}
